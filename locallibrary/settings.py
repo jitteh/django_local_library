e43acae19ec3edb4e91fbcad3ec07947fb22888d
kjitteh <jitteh10@gmail.com>
2022-01-20 16:53:50 +0800

    update ALLOWED_HOSTS with site and development server URL

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -22,13 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-k)9&-u3e@!@_5)a^+zbnx8lusn^cfe6hk+eoqib9tf6)_)t4zo'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-k)9&-u3e@!@_5)a^+zbnx8lusn^cfe6hk+eoqib9tf6)_)t4zo')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ancient-river-37299.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
